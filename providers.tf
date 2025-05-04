e38d47e1f6dbb9d6d7af0cc8aa039a533dfc482d
NEPatriots-Coder <lamar.wells@smail.rasmussen.edu>
2025-05-04 17:12:06 -0500

    Updated required versons to align with open tofu

--- providers.tf
+++ providers.tf
@@ -1,10 +1,9 @@
 terraform {
+  required_version = ">= 1.5.0, < 1.12.0" # Allow versions compatible with OpenTofu 1.6.3 and Terraform 1.11.x
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
   }
-  required_version = "~> 1.11.4"
 }
-
