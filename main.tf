757e9310b4739db7c762eb1fcfc8b9b8129a62d3
NEPatriots-Coder <lamar.wells@smail.rasmussen.edu>
2025-05-04 16:07:33 -0500

    Updated main.tf with more robust security

--- main.tf
+++ main.tf
@@ -8,6 +8,53 @@ resource "aws_vpc" "main" {
   }
 }
 
+# Create VPC Flow Logs
+resource "aws_flow_log" "main" {
+  iam_role_arn    = aws_iam_role.flow_log.arn
+  log_destination = aws_cloudwatch_log_group.flow_log.arn
+  traffic_type    = "ALL"
+  vpc_id          = aws_vpc.main.id
+}
+
+resource "aws_cloudwatch_log_group" "flow_log" {
+  name = "vpc-flow-logs"
+}
+
+resource "aws_iam_role" "flow_log" {
+  name = "vpc-flow-log-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Principal = {
+        Service = "vpc-flow-logs.amazonaws.com"
+      },
+      Effect = "Allow"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "flow_log" {
+  name = "vpc-flow-log-policy"
+  role = aws_iam_role.flow_log.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents",
+        "logs:DescribeLogGroups",
+        "logs:DescribeLogStreams"
+      ],
+      Effect   = "Allow",
+      Resource = "*"
+    }]
+  })
+}
+
 # Create an Internet Gateway
 resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.main.id
@@ -20,7 +67,7 @@ resource "aws_internet_gateway" "gw" {
 resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
-  map_public_ip_on_launch = true
+  map_public_ip_on_launch = false  # Changed to false for security
   availability_zone       = "us-east-1a"
   tags = {
     Name = "hello-world-subnet"
@@ -52,10 +99,19 @@ resource "aws_security_group" "instance" {
   vpc_id      = aws_vpc.main.id
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow HTTPS outbound traffic to specific ranges only"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"] # Restricted to VPC CIDR range
+  }
+  
+  egress {
+    description = "Allow HTTP outbound traffic to specific ranges only"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"] # Restricted to VPC CIDR range
   }
 
   tags = {
@@ -69,6 +125,17 @@ resource "aws_instance" "hello_world" {
   subnet_id              = aws_subnet.public.id
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.instance.id]
+  
+  # Added metadata_options to require IMDSv2 tokens
+  metadata_options {
+    http_tokens = "required"
+    http_endpoint = "enabled"
+  }
+  
+  # Added root block device encryption
+  root_block_device {
+    encrypted = true
+  }
 
   tags = {
     Name = "HelloWorld"
